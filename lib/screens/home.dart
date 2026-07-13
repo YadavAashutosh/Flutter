961f1f085786c28ac20bb6b8dbfb2611797be161
Ashu <ashuyadav210905@gmail.com>
2026-07-13 20:34:15 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,45 +1,44 @@
 import "package:flutter/material.dart";
 import "count.dart";
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-@override
-Widget build(BuildContext context){
-    return  Scaffold(
-        appBar : AppBar( 
-          leading : Icon(Icons.menu),
-          title : Row (
-             
-            mainAxisAlignment : MainAxisAlignment.center,
-    
-           children : [ 
-            
-               Icon(Icons.home),
-               SizedBox(width: 8),
-               GestureDetector(
-                  onTap : () {
-                    Navigator.push(
-                      context ,
-                        MaterialPageRoute(
-                          builder : (context) => const Countpage()
-                                         ),
-                                  );
-                  },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-               child:  Text("Ashu Home Page , Press this to navigate")
-                                )
-        ],
+          children: [
+            Icon(Icons.home),
+            SizedBox(width: 8),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Countpage()),
+                );
+              },
+
+              child: Text(
+                "Ashu Home Page , Press this to navigate",
+                style: TextStyle(
+                  color: Colors.deepOrangeAccent,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
 
-        actions : [ CircleAvatar( backgroundColor: Colors.blue, child : Text("A") )],
-        backgroundColor : Colors.amber,
-        ),
-        body: Center(
-          child : Text("Hello I m Ashu "),
-        ),
-        
+        actions: [CircleAvatar(backgroundColor: Colors.blue, child: Text("A"))],
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(child: Text("Hello I m Ashu ")),
     );
-}}
-
+  }
+}
